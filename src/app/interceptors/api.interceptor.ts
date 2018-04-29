a3d88348ec493db6a241649ef644707379aff720
Marta <martavivesluis@gmail.com>
2018-04-29 20:40:11 +0200

    modify the header

--- src/app/interceptors/api.interceptor.ts
+++ src/app/interceptors/api.interceptor.ts
@@ -5,7 +5,7 @@ import {Observable} from 'rxjs/Observable';
 @Injectable()
 export class APIInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-// add the token 
+// add the token
     const apiReq = req.clone({ url: `http://localhost:3000/${req.url}` });
     return next.handle(apiReq);
   }
