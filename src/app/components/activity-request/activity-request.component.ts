4ea9d66d6fd7d991bf05e71c8991ddf589fb5ca3
AnnaDiazPineda <31926227+AnnaDiazPineda@users.noreply.github.com>
2018-05-07 10:07:18 +0200

    ++eliminar i acceptar, botto

--- src/app/components/activity-request/activity-request.component.ts
+++ src/app/components/activity-request/activity-request.component.ts
@@ -5,6 +5,8 @@ import {ActivityRequest}          from '../../models/activityRequest.model'
 import {ActivityRequestResponse}  from '../../models/activityRequestResponse.model'
 import {ActivityService} from '../../service/activity.service';
 import {resolveProjectModule} from '@angular/cli/utilities/require-project-module';
+import {error} from 'util';
+import {BehaviorSubject} from 'rxjs/BehaviorSubject';
 
 @Component({
   selector: 'app-activity-request',
@@ -17,10 +19,15 @@ export class ActivityRequestComponent implements OnInit {
   public activityRequest: ActivityRequest;
     myPetition:    ActivityRequestResponse[];
     theirPetition: ActivityRequestResponse[];
+    counters;
 
-  constructor(private actvitiyRequestService : ActivityRequestService) {
+
+
+
+    constructor(private actvitiyRequestService : ActivityRequestService) {
 
     this.actvitiyRequestService.getMyPetitions(localStorage.userId).subscribe(
+
 
       response => {
         if (response) {
@@ -46,13 +53,47 @@ export class ActivityRequestComponent implements OnInit {
       }
     );
 
-  }
+
+      this.actvitiyRequestService.getCounters(localStorage.userId).subscribe(
+        response => {
+          if(response) {
+            console.log(response)
+            this.counters = response;
+          }
+        },
+        error=>{
+
+        }
+      );
+    }
 
 
   ngOnInit() {
   }
+  deletePetition(id) {
+    const response = confirm("estas segur d'esborrar la petició?")
+    if (response) {
+      console.log(id);
+      const myPetition = this.myPetition; //arreglo
+      this.actvitiyRequestService.deletePetition(id).subscribe(
+        data => {
+          if (data) {
 
-
+            console.log(data)
+          }
+        },
+        error => {
+          console.log(<any>error);
+        }
+      );
+    }
+  }return;
 
 
 }
+
+
+
+
+
+
