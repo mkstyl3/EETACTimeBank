81e96fe05d7e14da71f03a20d2b277f80b3f9dac
Oscar Mampel <oscar@aaaida.com>
2018-05-07 19:55:28 +0200

    corregida variable de entorn

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   urlBackend: 'http://147.83.7.155:3000',
-  urlFrontend: 'http://localhost:4200',
+  urlFrontend: 'http://147.83.7.155',
   urlChat: 'http://147.83.7.155:8880'
 };
