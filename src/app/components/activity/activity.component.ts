c6f01e061bffe3880ad5ec8fbe93f5d6c36f4a30
AnnaDiazPineda <31926227+AnnaDiazPineda@users.noreply.github.com>
2018-05-10 12:35:41 +0200

    ++ novetats. falta funcionalitats dels botons

--- src/app/components/activity/activity.component.ts
+++ src/app/components/activity/activity.component.ts
@@ -3,6 +3,7 @@ import {Activity} from '../../models/activity.model';
 import {User} from '../../models/user.model';
 import {NovetatsResponse} from '../../models/novetatsResponse'
 import {ActivityService} from '../../service/activity.service';
+import { UserService } from '../../service/user.service';
 import { Router, Params, ActivatedRoute } from '@angular/router';
 import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
@@ -22,13 +23,30 @@ export class ActivityComponent implements OnInit {
   public novetats: NovetatsResponse[];
 
 
-  constructor(private activityService: ActivityService) {
+  constructor(private activityService: ActivityService, private userService: UserService) {
     this.activity = new Activity('', 10, 10, 0, localStorage.username, '', '');
+
+    this.activityService.getNovetats().subscribe(
+      response=> {
+        if(response){
+          console.log(response)
+          this.novetats = response;
+        }
+      },
+      error=> {
+        console.log(<any>error);
+      }
+    );
+
   }
 
   ngOnInit() { }
 
   addTag(tag: string) { this.activity.tags.push(tag); }
+
+  veurePerfil(name:string){
+      this.userService.getProfileUser$(name)
+  }
 
   onSubmit() {
     console.log(this.activity);
