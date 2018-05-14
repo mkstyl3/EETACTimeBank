48b5a02cbed26a5cc6cfe1302ecd665b232fea2e
Oscar Mampel <oscar@aaaida.com>
2018-05-14 19:26:33 +0200

    ups

--- src/app/interceptors/api.interceptor.ts
+++ src/app/interceptors/api.interceptor.ts
@@ -21,7 +21,7 @@ export class APIInterceptor implements HttpInterceptor {
     
     const authorization = this.authService.getToken();
     const apiReq = req.clone({ 
-      url: environment.urlBackend`${req.url}`,
+      url: environment.urlBackend`/${req.url}`,
       headers: req.headers.set('Authorization', authorization) 
     });
     return next.handle(apiReq)
