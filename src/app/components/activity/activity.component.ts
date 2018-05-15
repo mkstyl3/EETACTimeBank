9563e1602857772b19bd3c376401b02c43223cea
alberti-tu <albertalvarezmiron@gmail.com>
2018-05-16 01:11:09 +0200

    HOME

--- src/app/components/activity/activity.component.ts
+++ src/app/components/activity/activity.component.ts
@@ -1,47 +1,54 @@
 import { Component, OnInit } from '@angular/core';
-import {Activity} from '../../models/activity.model';
-import {User} from '../../models/user.model';
-import {NovetatsResponse} from '../../models/novetatsResponse'
-import {ActivityService} from '../../service/activity.service';
+import { Activity } from '../../models/activity.model';
+import { User } from '../../models/user.model';
+import { ActivityService } from '../../service/activity.service';
 import { UserService } from '../../service/user.service';
-import { Router, Params, ActivatedRoute } from '@angular/router';
-import {FormsModule} from '@angular/forms';
-import {HttpModule} from '@angular/http';
-import {HttpErrorResponse} from '@angular/common/http';
-import {ActivityRequest} from '../../models/activityRequest.model';
-import {isDate} from 'util';
-import {DateFormatter} from '@angular/common/src/pipes/deprecated/intl';
-
+import { HttpErrorResponse } from '@angular/common/http';
+import { ActivityRequest } from '../../models/activityRequest.model';
 
 @Component({
   selector: 'app-activity',
   templateUrl: './activity.component.html',
   styleUrls: ['./activity.component.css'],
-  providers: [ActivityService]
+  providers: [ ActivityService ]
 })
 export class ActivityComponent implements OnInit {
 
+  showModalUser: boolean;
+  showModalActivity: boolean;
+  showModalPetition: boolean;
 
-  public activity: Activity;
-  public activitySelect: Activity;
-  public activityRequest: ActivityRequest;
-  public user: User;
-  public novetats: NovetatsResponse[];
+  activity: Activity;
+  novetats: Activity;
+  activitySelect: Activity;
+  activityRequest: ActivityRequest;
+  user: User;
 
+  latitud_map: number;
+  longitud_map: number;
+  latitud_marker_user: number;
+  longitud_marker_user: number;
+  latitud_marker_activity: number;
+  longitud_marker_activity: number;
+  showMap: boolean;
 
   constructor(private activityService: ActivityService, private userService: UserService) {
     this.activity = new Activity('', 41.275443, 1.98665, 0, localStorage.username, '', '');
+    this.showMap = false;
+    this.showModalUser = false;
+    this.showModalActivity = false;
+    this.showModalPetition = false;
   }
 
   ngOnInit() {
-    this.activityService.getNovetats().subscribe(
-      response=> {
-        if(response){
-          console.log(response)
-          this.novetats = response;
+    this.activityService.getActivityAll().subscribe(
+      response => {
+        if (response) {
+          this.novetats = response; // Retorna totes les activitats
+          console.log(this.novetats);
         }
       },
-      error=> {
+      error => {
         console.log(<any>error);
       }
     );
@@ -49,30 +56,45 @@ export class ActivityComponent implements OnInit {
 
   addTag(tag: string) { this.activity.tags.push(tag); }
 
+  // Mostra el modal d'un usuari
   veurePerfil(name: string) {
-      this.userService.getProfileUser$(name).subscribe(
-        data => {
-          this.user = data;
-          console.log(this.user);
-          },
-        (err: HttpErrorResponse) => {
-          console.log(err);
-        }
-      );
+    this.showModalUser = false;
+    this.userService.getProfileUser$(name).subscribe(
+    data => {
+        this.user = data;
+        console.log(this.user);
+      },
+      (err: HttpErrorResponse) => {
+        console.log(err);
+      }
+    );
+    this.showModalUser = true;
   }
 
-  getActivity(activitySelect: Activity){
-    /*this.activityService.getActivity(id).subscribe(
-      data=>{
-        this.activity = data;
-        console.log(this.activity);
-      },
-      (err: HttpErrorResponse)=>
-      {
-        console.log(err)
-      }
-    )*/
-    this.activitySelect = activitySelect;
+  // Mostra el modal de una Activitat
+  getActivity(activity: Activity) {
+    this.showModalActivity = false;
+    this.activitySelect = activity;
+    console.log(this.activitySelect);
+
+    // Prepara el mapa
+    this.showMap = false;
+    this.latitud_map = this.activitySelect.latitude;
+    this.longitud_map = this.activitySelect.longitude;
+    this.latitud_marker_activity = this.activitySelect.latitude;
+    this.longitud_marker_activity = this.activitySelect.longitude;
+
+    // Posición del Usuario
+    const self = this;
+    if (navigator) {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        // Devuelve los valores del CallBack
+        self.latitud_marker_user = position.coords.latitude;
+        self.longitud_marker_user = position.coords.longitude;
+        self.showMap = true;
+      });
+    } else { console.error('Error: No se puede acceder a la localización'); }
+    this.showModalActivity = true;
   }
 
   makeApetition(ToName, idActivity) {
@@ -81,10 +103,8 @@ export class ActivityComponent implements OnInit {
   }
 
   sendAPetition(activityRequest) {
-
     console.log(this.activityRequest);
     this.activityService.makeApetition(this.activityRequest).subscribe(
-
       response => {
         if (response) {
           console.log(response);
@@ -110,8 +130,4 @@ export class ActivityComponent implements OnInit {
       }
     );
   }
-
-
-
-
 }
