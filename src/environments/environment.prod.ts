9bb67464f113aedfa5eaacaf92e5258d3fd3871d
Oscar Mampel <oscar@aaaida.com>
2018-05-28 14:03:25 +0200

    ara aniras?

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   urlBackend: 'https://backend.bancdetemps.tk',
   urlFrontend: 'https://bancdetemps.tk',
-  urlChat: 'https://chat.bancdetemps.tk'
+  urlChat: 'http://chat.bancdetemps.tk'
 };
